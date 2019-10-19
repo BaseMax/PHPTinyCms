6996566b2343082f2543854adb2b6b20b9d99974
Max Base <Max@Asrez.com>
2019-10-20 01:47:53 +0330

    Your commit message

---
+++ _config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @Name : PHPTinyCms
+ * @Version : 1.0
+ * @Programmer : Max
+ * @Date : 2019-10-25
+ * @Released under : https://github.com/BaseMax/PHPTinyCms/blob/master/LICENSE
+ * @Repository : https://github.com/BaseMax/PHPTinyCms
+ *
+ **/
+if(count(get_included_files()) == ((version_compare(PHP_VERSION, '5.0.0', '>='))?1:0) ){exit('Restricted Access!');}
+$db->connect("localhost", "vistargo_cms", "Kz~i{l5-6L6-");
+$db->db="“vistargo_cms”";
---
+++ search.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * @Name : PHPTinyCms
+ * @Version : 1.0
+ * @Programmer : Max
+ * @Date : 2019-10-25
+ * @Released under : https://github.com/BaseMax/PHPTinyCms/blob/master/LICENSE
+ * @Repository : https://github.com/BaseMax/PHPTinyCms
+ *
+ **/
+include "_init.php";
+?>
