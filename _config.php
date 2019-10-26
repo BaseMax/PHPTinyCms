1353ef12094f77aa07eba08b2e1a191e476ce23b
Max Base <Max@Asrez.com>
2019-10-27 02:09:41 +0330

    Change database, host password!

--- _config.php
+++ _config.php
@@ -10,5 +10,5 @@
  *
  **/
 if(count(get_included_files()) == ((version_compare(PHP_VERSION, '5.0.0', '>='))?1:0) ){exit('Restricted Access!');}
-$db->connect("localhost", "vistargo_cms", "Kz~i{l5-6L6-");
-$db->db="vistargo_cms";
+$db->connect("localhost", "host_username", "host_pass");
+$db->db="database_name";
