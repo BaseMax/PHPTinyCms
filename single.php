2f3cd7c25ffbd9618f692ed806b111db9819f034
Max Base <MaxBaseCode@Gmail.Com>
2020-10-20 01:47:36 +0330

    Solve Undefined Variables #1

--- single.php
+++ single.php
@@ -1,10 +1,10 @@
-<?
+<?php
 /**
  *
  * @Name : PHPTinyCms
  * @Version : 1.0
  * @Programmer : Max
- * @Date : 2019-10-25
+ * @Date : 2019-10-25, 2020-10-20
  * @Released under : https://github.com/BaseMax/PHPTinyCms/blob/master/LICENSE
  * @Repository : https://github.com/BaseMax/PHPTinyCms
  *
@@ -14,9 +14,17 @@ if(!isset($_GET['id'])) {
     exit();
 }
 $id=$_GET['id'];
+$count=$db->count("post", ["id"=>$id]);
+if($count == 0 || $count == null) {
+    exit("Not found!");    
+}
+else {
 $post=$db->select("post", ["id"=>$id]);
 ?>
-<h1><a href="single.php?id=<?= $post["id"] ?>"><?= $post["subject"] ?></a></h1>
-<p><?= $post["text"] ?></p>
-<p><?= $post["texts"] ?></p>
-<br>
+    <h1><a href="single.php?id=<?= $post["id"] ?>"><?= $post["subject"] ?></a></h1>
+    <p><?= $post["text"] ?></p>
+    <p><?= $post["texts"] ?></p>
+    <br>
+<?php
+}
+?>
